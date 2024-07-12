d42ab6f20b82e852998ccd200e8cbeb840972a62
xkuvvetmira <kuvvetmira@null.net>
2024-07-12 21:02:55 +0300

    upload htdocs files

---
+++ index.php
@@ -0,0 +1,195 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
+    header('Location: login.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Duyurular</title>
+    <!-- Bootstrap CSS -->
+    <style>
+        .inputicin {
+            background-color: white;
+            border-radius: 15px;
+            font-size: 15px;
+            width: 180px;
+            height: 23px;
+            margin-left: 154px;
+        }
+        .inputicinn {
+            background-color: white;
+            border-radius: 15px;
+            font-size: 15px;
+            width: 240px;
+            height: 23px;
+            margin-left: 126px;
+        }
+        .kutu {
+            background-color: rgba(0, 0, 0, 0.5);
+            width: 1350px;
+            height: 631px;
+            border-radius: 15px;
+            box-shadow: 1px 1px 40px white;
+        }
+        label {
+            color: white;
+            font-family: Arial;
+            margin-left: 148px;
+        }
+        h2 {
+            color: white;
+            font-family: Arial;
+            margin-left: 185px;
+        }
+        body {
+            background-color: black;
+            background-image: url(https://media.istockphoto.com/id/537761159/photo/human-skull.jpg?s=612x612&w=0&k=20&c=yu6S06Fhrb5aOWbdWlIeEUppgXC4_luoYpL4-ZXR6NQ=);
+            background-size: 150px;
+        }
+        .buton {
+            color: black;
+            background-color: grey;
+            text-align: center;
+            border-style: none;
+            border-radius: 50px;
+            font-size: 18px;
+            width: 180px;
+            height: 40px;
+            margin-left: 160px;
+            transition: 390ms;
+        }
+        .buton:hover {
+            box-shadow: 1px 1px 15px white;
+        }
+        .buton2 {
+            color: black;
+            background-color: grey;
+            text-align: center;
+            border-style: none;
+            text-decoration: none;
+            border-radius: 50px;
+            padding: 10px;
+            font-family: Arial;
+            font-size: 18px;
+            width: 200px;
+            height: 40px;
+            transition: 390ms;
+            margin-left: 185px;
+        }
+        .buton2:hover {
+            box-shadow: 1px 1px 15px white;
+        }
+        .sidebar {
+            width: 250px;
+            height: 100%;
+            position: fixed;
+            top: 0;
+            left: 0;
+            background-color: black;
+            overflow-x: hidden;
+            padding-top: 20px;
+            font-family: Arial;
+            font-size: 20px;
+            border-right: 1px solid #fff;
+        }
+
+        .menu {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        .menu li {
+            padding: 10px 15px;
+            border-bottom: 1px solid #ccc;
+        }
+
+        .menu li a {
+            text-decoration: none;
+            color: #fff;
+            display: block;
+            padding: 10px;
+        }
+
+        .menu li a:hover {
+            background-color: #ddd;
+            color: #000;
+        }
+        h3 {
+            color: white;
+            font-family: Arial;
+            font-size: 25px;
+        }
+        .duyurular {
+            background-color: black;
+            box-shadow: 1px 1px 20px white;
+            border-radius: 40px;
+            width: 500px;
+            height: 400px;
+            margin-left: 530px;
+            font-family: Arial;
+            color: white;
+            padding: 10px 15px;
+            border-bottom: 1px solid #ccc;
+        }
+    </style>
+</head>
+<body>
+<br><div id="datetime" style="font-size: 23px; text-align: center; margin-left: 660px; color: white; font-family:Verdana, Geneva, Tahoma, sans-serif; background-color: black; width: 285px; border-radius: 10px; box-shadow: 1px 1px 15px grey;"></div>
+<br><br><a style="font-size: 50px; text-align: center; margin-left: 650px; color: white; font-family:Verdana, Geneva, Tahoma, sans-serif; background-color: black; width: 175px; border-radius: 10px; box-shadow: 1px 1px 15px grey; padding: 10px;">Hoşgeldiniz</a>
+<div class="duyurular">
+<br><a style="margin-left: 195px; font-size: 30px;">Duyurular</a>
+<br><a style="margin-left: 30px; font-size: 10px;">▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂▂</a>
+<br><br>12.07.2024 ● Yapımcılar: <a href="https://crackturkey.com/uyeler/kuvvetmira.183452/" style="text-decoration: none; color: red; text-shadow: 1px 1px 15px white">Kuvvetmira</a>, <a href="https://crackturkey.com/uyeler/freaksven.61668/" style="text-decoration: none; color: violet; text-shadow: 1px 1px 15px white">Freaksven</a>
+<br><br>12.07.2024 ● <a href="https://crackturkey.com/" style="text-decoration: none; color: cyan; font-style: italic;">crackturkey.com</a> | <a href="https://discord.gg/crackturkey" style="text-decoration: none; color: cyan; font-style: italic;">.gg/crackturkey</a>
+<br><br>12.07.2024 ● Discord Adreslerimiz: <a style="text-decoration: none; color: red; text-shadow: 1px 1px 15px white">freakxd33</a>, <a style="text-decoration: none; color: violet; text-shadow: 1px 1px 15px white">kuvvetmira.sago</a>
+</div>
+<div class="sidebar">
+    <ul class="menu">
+        <a style="color: white;">☰ Menü</a>
+        <li><a href="/">🏠 Dashboard</a></li>
+        <li><a href="process.php">📞 Tr Tell Pass Ayırıcı</a></li>
+        <li><a href="process2.php">📄 Txt Log Extractor</a></li>
+		<li><a href="process3.php">🔗 Url Silici</a></li>
+        <li><a href="process4.php">📚 Duplicates Silici</a></li>
+    </ul>
+</div>
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script>
+                    function getDateTime() {
+                        const apiUrl = 'https://worldtimeapi.org/api/timezone/Europe/Istanbul';
+                        fetch(apiUrl)
+                            .then(response => response.json())
+                            .then(data => {
+                                const datetime = new Date(data.datetime);
+                                const formattedDateTime = datetime.toLocaleString('en-GB', {
+                                    year: 'numeric',
+                                    month: '2-digit',
+                                    day: '2-digit',
+                                    hour: '2-digit',
+                                    minute: '2-digit',
+                                    second: '2-digit'
+                                }).replace(/\//g, '.');
+                
+                                const datetimeElement = document.getElementById('datetime');
+                                datetimeElement.textContent = formattedDateTime;
+                            })
+                            .catch(error => {
+                                console.error('API ile ilgili bir hata oluştu:', error);
+                            });
+                    }
+                    getDateTime();
+                    setInterval(getDateTime, 1000);
+                </script>                                
+</body>
+</html>
